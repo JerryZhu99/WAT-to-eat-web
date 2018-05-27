a10cbee20aea26c5060a8d801e279274ef1ee17c
Jerry Zhu <jerryzhu99@gmail.com>
2018-05-26 22:20:45 -0400

    Modify welcome screen

--- src/component/WelcomeScreen.jsx
+++ src/component/WelcomeScreen.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Button, Container, Jumbotron } from 'reactstrap';
+import Auth from '../Auth';
+import { Redirect } from 'react-router-dom';
 
 class WelcomeScreen extends Component {
     render() {
+        if (Auth.signedIn) return <Redirect to="/dashboard" />
+
         return (
             <div className="Welcome">
                 <Container>
-                    <Jumbotron>
+                    <Jumbotron className="mt-3">
                         <h1 className="display-3">Budget Proactively.</h1>
                         <p className="lead">WAT to Eat uses your spending habits to recommend you food within your budget, and lets you know when you go over.</p>
                         <hr className="my-2" />
