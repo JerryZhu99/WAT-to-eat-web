e696a70aa0d46c41e7477ce256a3aa4d7d18d639
Jerry Zhu <jerryzhu99@gmail.com>
2018-05-26 23:18:40 -0400

    Improved welcome screen

--- src/component/WelcomeScreen.jsx
+++ src/component/WelcomeScreen.jsx
@@ -3,7 +3,10 @@ import { Button, Container, Jumbotron } from 'reactstrap';
 import Auth from '../Auth';
 import { Redirect } from 'react-router-dom';
 
+import './WelcomeScreen.css'
+
 class WelcomeScreen extends Component {
+
     render() {
         if (Auth.signedIn) return <Redirect to="/dashboard" />
 
@@ -15,12 +18,54 @@ class WelcomeScreen extends Component {
                         <p className="lead">WAT to Eat uses your spending habits to recommend you food within your budget, and lets you know when you go over.</p>
                         <hr className="my-2" />
                         <p className="lead">
-                            <Button color="primary">Get Started</Button>
+                            <Button color="success">Get Started</Button>
                         </p>
                     </Jumbotron>
                     <h1>
-                        How it works
+                        Features
                     </h1>
+                    <div className="row">
+                        <div className="col-md-4 p-3">
+                            <div className="icon-wrapper w-100 d-flex flex-column align-items-center">
+                                <div className="icon">
+                                    <i class="fas fa-balance-scale fa-7x"></i>
+                                </div>
+                            </div>
+
+                            <h2>Budgeted Suggestions</h2>
+                            <p>
+                                WAT to Eat lets you choose a meal that fits your budget.
+                                Whether you want to save or splurge, WAT to Eat suggests restaurants that fit the bill.
+                            </p>
+                        </div>
+
+                        <div className="col-md-4 p-3">
+
+                            <div className="icon-wrapper w-100 d-flex flex-column align-items-center">
+                                <div className="icon">
+                                    <i class="fas fa-info-circle fa-7x"></i>
+                                </div>
+                            </div>
+
+
+                            <h2>Friendly Reminders</h2>
+                            <p>
+                                WAT to Eat informs you of the average meal prices when you visit a restaurant above your budget.
+                            </p>
+                        </div>
+
+                        <div className="col-md-4 p-3">
+                            <div className="icon-wrapper w-100 d-flex flex-column align-items-center">
+                                <div className="icon">
+                                    <i class="fas fa-chart-bar fa-7x"></i>
+                                </div>
+                            </div>
+                            <h2>Data Breakdowns</h2>
+                            <p>
+                                WAT to Eat breaks down your spending habits and helps you set goals for the future.
+                            </p>
+                        </div>
+                    </div>
                 </Container>
             </div>
         );
